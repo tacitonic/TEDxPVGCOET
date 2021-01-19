e32eef8f865c97d888bffa73bec875d7f41abe59
dvrohan <dvrohan04@gmail.com>
2021-01-19 17:37:07 +0530

    Added popup in events page

---
+++ js/custom-script.js
@@ -0,0 +1,25 @@
+/* Template: Evolo - StartUp HTML Landing Page Template
+   Author: Inovatik
+   Created: June 2019
+   Description: Custom JS file
+*/
+
+
+(function($) {
+    "use strict"; 
+	
+	/* Lightbox - Magnific Popup */
+	$('.popup-with-move-anim').magnificPopup({
+		type: 'inline',
+		fixedContentPos: false, /* keep it false to avoid html tag shift with margin-right: 17px */
+		fixedBgPos: true,
+		overflowY: 'auto',
+		closeBtnInside: true,
+		preloader: false,
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-slide-bottom'
+	});
+    
+
+})(jQuery);
