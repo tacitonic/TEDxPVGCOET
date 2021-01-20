9f9c65e64aa242ad02838813276660b042b16393
dvrohan <dvrohan04@gmail.com>
2021-01-20 13:20:40 +0530

    Adjusted the popup size

--- js/custom-script.js
+++ js/custom-script.js
@@ -5,9 +5,41 @@
 */
 
 
-(function($) {
-    "use strict"; 
-	
+(function ($) {
+	"use strict";
+
+	/* Video Lightbox - Magnific Popup */
+	$('.popup-youtube, .popup-vimeo').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false,
+		iframe: {
+			patterns: {
+				youtube: {
+					index: 'youtube.com/',
+					id: function (url) {
+						var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
+						if (!m || !m[1]) return null;
+						return m[1];
+					},
+					src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+				},
+				vimeo: {
+					index: 'vimeo.com/',
+					id: function (url) {
+						var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
+						if (!m || !m[5]) return null;
+						return m[5];
+					},
+					src: 'https://player.vimeo.com/video/%id%?autoplay=1'
+				}
+			}
+		}
+	});
+
 	/* Lightbox - Magnific Popup */
 	$('.popup-with-move-anim').magnificPopup({
 		type: 'inline',
@@ -20,6 +52,6 @@
 		removalDelay: 300,
 		mainClass: 'my-mfp-slide-bottom'
 	});
-    
+
 
 })(jQuery);
